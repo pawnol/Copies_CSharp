f099b65a45449efc04c594cfba55b5e6249a25bf
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-12-14 10:42:46 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Copies
+{
+    partial class CopiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.copiesLabel = new System.Windows.Forms.Label();
+            this.copiesTextBox = new System.Windows.Forms.TextBox();
+            this.calculateCostButton = new System.Windows.Forms.Button();
+            this.totalCostLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // copiesLabel
+            // 
+            this.copiesLabel.AutoSize = true;
+            this.copiesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copiesLabel.Location = new System.Drawing.Point(62, 39);
+            this.copiesLabel.Name = "copiesLabel";
+            this.copiesLabel.Size = new System.Drawing.Size(165, 24);
+            this.copiesLabel.TabIndex = 0;
+            this.copiesLabel.Text = "Number of copies:";
+            // 
+            // copiesTextBox
+            // 
+            this.copiesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copiesTextBox.Location = new System.Drawing.Point(233, 36);
+            this.copiesTextBox.Name = "copiesTextBox";
+            this.copiesTextBox.Size = new System.Drawing.Size(100, 29);
+            this.copiesTextBox.TabIndex = 1;
+            // 
+            // calculateCostButton
+            // 
+            this.calculateCostButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateCostButton.Location = new System.Drawing.Point(66, 86);
+            this.calculateCostButton.Name = "calculateCostButton";
+            this.calculateCostButton.Size = new System.Drawing.Size(267, 55);
+            this.calculateCostButton.TabIndex = 2;
+            this.calculateCostButton.Text = "Calculate Total Cost";
+            this.calculateCostButton.UseVisualStyleBackColor = true;
+            this.calculateCostButton.Click += new System.EventHandler(this.calculateCostButton_Click);
+            // 
+            // totalCostLabel
+            // 
+            this.totalCostLabel.AutoSize = true;
+            this.totalCostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCostLabel.Location = new System.Drawing.Point(65, 162);
+            this.totalCostLabel.Name = "totalCostLabel";
+            this.totalCostLabel.Size = new System.Drawing.Size(132, 24);
+            this.totalCostLabel.TabIndex = 3;
+            this.totalCostLabel.Text = "That will cost $";
+            // 
+            // CopiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 223);
+            this.Controls.Add(this.totalCostLabel);
+            this.Controls.Add(this.calculateCostButton);
+            this.Controls.Add(this.copiesTextBox);
+            this.Controls.Add(this.copiesLabel);
+            this.Name = "CopiesForm";
+            this.Text = "Copies";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label copiesLabel;
+        private System.Windows.Forms.TextBox copiesTextBox;
+        private System.Windows.Forms.Button calculateCostButton;
+        private System.Windows.Forms.Label totalCostLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Copies
+ * Pawelski
+ * 12/14/2021
+ * This program calculates the total cost to make copies
+ * when there is a flat fee of $5 and each copy costs
+ * $0.01.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Copies
+{
+    public partial class CopiesForm : Form
+    {
+        public CopiesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateCostButton_Click(object sender, EventArgs e)
+        {
+            int numberOfCopies;
+            double totalCost;
+            numberOfCopies = Convert.ToInt32(copiesTextBox.Text);
+            totalCost = numberOfCopies * 0.01 + 5.0;
+            totalCostLabel.Text = "That will cost $" + totalCost + ".";
+        }
+    }
+}
